0d52d2d52a6134ff49bc6acaba5fd0ef7650db76
Samuel Gutierrez Sanchez <samuelgtz025@gmail.com>
2024-09-09 18:15:18 -0600

    personaje con raycast y prefab

---
+++ Assets/Scenes/RaycastFromCamera.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class RaycastFromCamera : MonoBehaviour
+{
+    public Camera cam; // Asigna tu cámara en el inspector
+
+    void Update()
+    {
+        // Lanza el raycast desde el centro de la cámara
+        Ray ray = cam.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0));
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            // Si el raycast golpea un GameObject
+            Renderer renderer = hit.transform.GetComponent<Renderer>();
+            if (renderer != null)
+            {
+                if (hit.transform.CompareTag("Enemy"))
+                {
+                    // Si el GameObject tiene el tag "Enemy", cambia su color a rojo
+                    renderer.material.color = Color.red;
+                }
+                else
+                {
+                    // Si el GameObject no tiene el tag "Enemy", cambia su color a gris oscuro
+                    renderer.material.color = Color.gray;
+                }
+            }
+        }
+    }
+}
